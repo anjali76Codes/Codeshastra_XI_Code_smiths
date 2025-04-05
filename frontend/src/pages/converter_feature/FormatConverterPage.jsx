4ef94568e83cc707d3b61594601317c8159402ca
unknown <sarwadeepdhaval@gmail.com>
2025-04-05 14:23:29 +0530

    Converter feature added

---
+++ frontend/src/pages/converter_feature/FormatConverterPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormatConverter from '../../components/converter_feature/FormatConverter'
+
+const FormatConverterPage = () => {
+  return (
+    <div>
+      <FormatConverter />
+    </div>
+  )
+}
+
+export default FormatConverterPage
