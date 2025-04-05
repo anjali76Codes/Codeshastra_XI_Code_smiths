f6203ef40ec702887c1036a09e767f4225dd8a1b
Krisha <chikkakrisha@gmail.com>
2025-04-05 14:27:14 +0530

    Image and Graphic Converter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import GraphicGenerator from './pages/ImgGraphicConverter/GraphicGenerator';
+import ImageConverter from './pages/ImgGraphicConverter/ImageConvertor';
+import ColorFeature from './pages/ImgGraphicConverter/Color';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <nav className="bg-black shadow-md p-4">
+        <div className="max-w-7xl mx-auto flex space-x-4">
+          <Link to="/" className="text-blue-600 hover:underline">Home</Link>
+          <Link to="/graphic" className="text-blue-600 hover:underline">graphic Generator</Link>
+          <Link to="/image" className="text-blue-600 hover:underline">Img converter</Link>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div className="text-center mt-10 text-xl">Welcome to the QR Code App</div>} />
+        <Route path="/graphic" element={<GraphicGenerator />} />
+        <Route path="/image" element={<ImageConverter />} />
+        <Route path="/color" element={<ColorFeature />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
