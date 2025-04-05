5006f42d89776351ffa56d4fe1ff876cbb1f9b7b
Anjali Gupta <152984851+anjali76Codes@users.noreply.github.com>
2025-04-05 15:37:20 +0530

    some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TextFormatter from './components/TextFormatter';
-import CodeFormatter from './components/CodeFormatter';
-import JsonFormatter from './components/JsonFormatter';
-import FormatterApp from './components/FormatterApp';
-
-
-
-
+import CodeFormatter from "./components/validator_feature/CodeFormatter";
+import FormatterApp from "./components/validator_feature/FormatterApp";
 
 function App() {
   return (
@@ -15,9 +9,9 @@ function App() {
       <div className="min-h-screen bg-gray-100 p-6">
         <main className="w-full max-w-5xl mx-auto">
           <Routes>
-            <Route path="/" element={<TextFormatter />} />
-            <Route path="/code" element={<CodeFormatter />} />
-            <Route path="/json" element={<JsonFormatter/>}/>
+            {/* <Route path="/" element={<TextFormatter />} /> */}
+            <Route path="/code" element={<CodeFormatter/>} />
+            {/* <Route path="/json" element={<JsonFormatter/>}/> */}
             <Route path="/format" element={<FormatterApp/>}/>
           </Routes>
         </main>
